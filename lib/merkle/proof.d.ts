3b79a7a82849051f48f0ea333f7cb93b0c8e501e
Steven Cedro <steven@southbanksoftware.com>
2021-04-14 13:34:40 +1000

    Config and package fixes.

---
+++ lib/merkle/proof.d.ts
@@ -0,0 +1,28 @@
+import * as anchor from "../anchor";
+import { Path } from "./merkle";
+/**
+ * Representation of a confirmed and complete proof.
+ */
+export interface Proof {
+    id: string;
+    anchorType: anchor.Anchor.Type;
+    format: anchor.Proof.Format;
+    metadata: any;
+    data: any;
+}
+/**
+ * Converts an anchor confirmed proof to a merkle proof.
+ * @param proof the anchor proof
+ * @returns the merkle proof
+ */
+export declare function fromAnchorProof(proof: anchor.Proof): Proof;
+/**
+ *
+ * @param proof the proof to add the path to
+ * @param hash the hash the path begins at
+ * @param algorithm the algorithm used to construct the path
+ * @param path the path to add
+ * @param label the label (description)
+ * @returns the updated proof
+ */
+export declare function addPathToProof(proof: Proof, hash: string, algorithm: string, path: Path[], label?: string): Proof;
